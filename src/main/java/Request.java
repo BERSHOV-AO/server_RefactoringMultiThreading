b114db974acbcfe41c81f8bb2848d7a276b0785a
Aleksey Bershov <bershov.aleksey@bk.ru>
2023-10-06 13:48:06 +0300

    intermediate correction Request.java

--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -1,4 +1,12 @@
 import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.List;
 
 public class Request {
@@ -19,6 +27,7 @@ public class Request {
     // NameValuePair - это специальная пара <Key, Value> которая используется для представления параметров
     // в HTTP-запросе, т.е. www.example.com?key=value.
     private List<NameValuePair> paramsList;
+
     public Request(String method, String path, List<String> headersList, List<NameValuePair> paramsList) {
         this.method = method;
         this.path = path;
@@ -38,5 +47,99 @@ public class Request {
 
     public String getPath() {
         return path;
+    }
+
+    static Request createRequest(BufferedInputStream in) throws IOException, URISyntaxException {
+        final List<String> methodsList = List.of(GET, POST);
+
+        // лимит на request line + заголовки
+        final var limit = 4096;
+        in.mark(limit);
+        final var buffer = new byte[limit];
+        final var read = in.read(buffer);
+
+        // ищем request line
+        final var requestLineDelimiter = new byte[]{'\r', '\n'};
+        final var requestLineEnd = indexOf(buffer, requestLineDelimiter, 0, read);
+        if (requestLineEnd == -1) {
+            return null;
+        }
+
+        // Зачитываем request line, он должен состоять из трех параметров
+        final var requestLine = new String(Arrays.copyOf(buffer, requestLineEnd)).split(" ");
+        if (requestLine.length != 3) {
+            return null;
+        }
+
+        // check for method
+        final var method = requestLine[0];
+        if (!methodsList.contains(method)) {
+            return null;
+        }
+        System.out.println(method);
+
+        final var path = requestLine[1];
+        if (!path.startsWith("/")) {          // проверить
+            return null;
+        }
+        System.out.println(path);
+
+        // ищем заголовки
+        final var headersDelimiter = new byte[]{'\r', '\n', '\r', '\n'};
+        final var headersStart = requestLineEnd + requestLineDelimiter.length;
+        final var headersEnd = indexOf(buffer, headersDelimiter, headersStart, read);
+        if (headersEnd == -1) {
+            return null;
+        }
+
+        // Отматываем на начало буфера
+        in.reset();
+        // Пропускаем requestLine
+        in.skip(headersStart);
+
+        final var headerBytes = in.readNBytes(headersEnd - headersStart);
+        List<String> headers = Arrays.asList(new String(headerBytes).split("\r\n"));
+
+        List<NameValuePair> params = URLEncodedUtils.parse(new URI(path), StandardCharsets.UTF_8);
+
+        return new Request(method, path, headers, params);
+    }
+
+    private static int indexOf(byte[] array, byte[] target, int start, int max) {
+
+        outer:
+        for (int i = start; i < max - target.length + 1; i++) {
+            for (int j = 0; j < target.length; j++) {
+                if (array[i + j] != target[j]) {
+                    continue outer;
+                }
+            }
+            return i;
+        }
+        return -1;
+    }
+
+    public List<NameValuePair> getQueryParams() {
+        return paramsList;
+    }
+
+    public List<String> getHeaders() {
+        return headersList;
+    }
+
+    public NameValuePair getQueryParam(String name) {
+        return getQueryParams().stream()
+                .filter(param -> param.getName().equalsIgnoreCase(name))
+                .findFirst().orElse(new NameValuePair() {
+                    @Override
+                    public String getName() {
+                        return name;
+                    }
+
+                    @Override
+                    public String getValue() {
+                        return "";
+                    }
+                });
     }
 }
